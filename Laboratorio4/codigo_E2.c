7a62bdbedb13a7ccd30cb4d2e21f3d323318543b
Billie-Cocaine <84209714+Billie-Cocaine@users.noreply.github.com>
2021-06-05 00:36:42 -0600

    Update codigo_E2.c

--- Laboratorio4/codigo_E2.c
+++ Laboratorio4/codigo_E2.c
@@ -22,7 +22,7 @@ int main ( ) {
 	int j = 1;
 
 	while ( !done ) {
-		printf("Ejecutando ciclo%i\n", j);
+		printf("Ejecutando asignación de memoria,  ciclo %i\n", j);
 		int n = 10000000;
 		int *a = (int *)malloc (n * sizeof(int)) ;
 		
@@ -39,6 +39,7 @@ int main ( ) {
 		for ( i = 0; i < n ; i ++){
 
 			free(a);
+			printf("Limpiando memoria asinada%i\n ", i);
 			int *a = (int *)malloc (n * sizeof(int)) ;
 		
 		}
